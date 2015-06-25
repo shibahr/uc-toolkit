51029b6df67c567a3bd5f6ef2d66de561d3c997d
Martin Sloan <martin.sloan@karma-tek.com>
2015-06-25 16:49:30 -0400

    add Itl base configs

---
+++ database/migrations/2015_06_25_194606_create_itls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('itls', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('phone_id')->unsigned();
+            $table->foreign('phone_id')->references('id')->on('phones');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('itls');
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,52 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
-    mix.less('app.less');
+    var dtPluginsDir = 'vendor/bower_components/datatables-plugins/';
+
+    // Copy jquery, bootstrap, and font awesome
+    mix.copy(
+            'vendor/bower_components/jquery/dist/jquery.js',
+            'resources/assets/js/jquery.js'
+        ).copy(
+            'vendor/bower_components/bootstrap/less',
+            'resources/assets/less/bootstrap'
+        ).copy(
+            'vendor/bower_components/bootstrap/dist/js/bootstrap.js',
+            'resources/assets/js/bootstrap.js'
+        ).copy(
+            'vendor/bower_components/bootstrap/dist/fonts',
+            'public/assets/fonts'
+        ).copy(
+            'vendor/bower_components/fontawesome/less',
+            'resources/assets/less/fontawesome'
+        ).copy(
+            'vendor/bower_components/fontawesome/fonts',
+            'public/assets/fonts'
+        );
+
+    // Copy datatables
+    mix.copy(
+            'vendor/bower_components/datatables/media/js/jquery.dataTables.js',
+            'resources/assets/js/dataTables.js'
+        ).copy(
+            dtPluginsDir + 'integration/bootstrap/3/dataTables.bootstrap.css',
+            'resources/assets/less/dataTables.less'
+        ).copy(
+            dtPluginsDir + 'integration/bootstrap/3/dataTables.bootstrap.js',
+            'resources/assets/js/dataTables.bootstrap.js'
+        );
+
+
+    // Combine scripts
+    mix.scripts([
+        'js/jquery.js',
+        'js/bootstrap.js',
+        'js/dataTables.js',
+        'js/dataTables.bootstrap.js'
+
+    ], 'public/assets/js/app.js', 'resources/assets');
+
+    // Compile Less
+    mix.less('app.less', 'public/assets/css');
+
 });
